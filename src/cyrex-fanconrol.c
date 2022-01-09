734f96bf40a90b657471c769f15fd1b602372efe
alxb421 <alxb421@gmail.com>
2022-01-09 18:36:19 +0200

    :tada: almost finished coding

---
+++ src/cyrex-fanconrol.c
@@ -0,0 +1,412 @@
+/*
+ ================================================================================
+ 
+ Based on https://github.com/SkyLandTW/clevo-indicator
+ ================================================================================
+ */
+
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/io.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+
+#include <libappindicator/app-indicator.h>
+
+#define NAME "cyrex-indicator"
+#define EC_SC 0x66
+#define EC_DATA 0x62
+#define IBF 1
+#define OBF 0
+#define EC_SC_READ_CMD 0x80
+#define EC_REG_SIZE 0x100
+#define EC_REG_CPU_TEMP 0x07
+#define EC_REG_GPU_TEMP 0xCD
+#define EC_REG_FAN_DUTY 0xCE
+#define EC_REG_FAN_RPMS_HI 0xD0
+#define EC_REG_FAN_RPMS_LO 0xD1
+#define MAX_FAN_RPM 4400.0
+
+typedef enum {
+    NA = 0, AUTO = 1, MANUAL = 2
+} MenuItemType;
+
+static void main_init_share(void);
+static int main_ec_worker(void);
+static void main_ui_worker(int argc, char** argv);
+static void main_on_sigchld(int signum);
+static void main_on_sigterm(int signum);
+static int main_dump_fan(void);
+static int show_temp(void);
+static int fan_speed(void);
+static int fan_duty(void);
+static int main_test_fan(int duty_percentage);
+static void ec_on_sigterm(int signum);
+static int ec_init(void);
+static int ec_auto_duty_adjust(void);
+static int ec_query_cpu_temp(void);
+static int ec_query_gpu_temp(void);
+static int ec_query_fan_duty(void);
+static int ec_query_fan_rpms(void);
+static int ec_write_fan_duty(int duty_percentage);
+static int ec_io_wait(const uint32_t port, const uint32_t flag,
+        const char value);
+static uint8_t ec_io_read(const uint32_t port);
+static int ec_io_do(const uint32_t cmd, const uint32_t port,
+        const uint8_t value);
+static int calculate_fan_duty(int raw_duty);
+static int calculate_fan_rpms(int raw_rpm_high, int raw_rpm_low);
+static void get_time_string(char* buffer, size_t max, const char* format);
+static void signal_term(__sighandler_t handler);
+
+struct {
+    volatile int exit;
+    volatile int cpu_temp;
+    volatile int gpu_temp;
+    volatile int fan_duty;
+    volatile int fan_rpms;
+    volatile int auto_duty;
+    volatile int auto_duty_val;
+    volatile int manual_next_fan_duty;
+    volatile int manual_prev_fan_duty;
+}static *share_info = NULL;
+
+static pid_t parent_pid = 0;
+
+int main(int argc, char* argv[]) {
+    if (ec_init() != EXIT_SUCCESS) {
+        printf("unable to control EC: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+    if (argc <= 1) {
+        char* display = getenv("DISPLAY");
+        if (display == NULL || strlen(display) == 0) {
+            return main_dump_fan();
+        } else {
+            parent_pid = getpid();
+            main_init_share();
+            signal(SIGCHLD, &main_on_sigchld);
+            signal_term(&main_on_sigterm);
+            pid_t worker_pid = fork();
+            if (worker_pid == 0) {
+                signal(SIGCHLD, SIG_DFL);
+                signal_term(&ec_on_sigterm);
+                return main_ec_worker();
+            } else if (worker_pid > 0) {
+                main_ui_worker(argc, argv);
+                share_info->exit = 1;
+                waitpid(worker_pid, NULL, 0);
+            } else {
+                printf("unable to create worker: %s\n", strerror(errno));
+                return EXIT_FAILURE;
+            }
+        }
+    } else {
+        if(strcmp(argv[1],"--temp")==0){
+	    return show_temp();
+            //return main_dump_fan();
+      	} else if(strcmp(argv[1],"--all")==0){
+      		return main_dump_fan();
+      	} else if(strcmp(argv[1],"--fan-speed")==0){
+      		return fan_speed();
+        } else if(strcmp(argv[1],"--fan-duty")==0){
+      		return fan_duty();
+        } else {
+            int val = atoi(argv[1]);
+            if (val < 10 || val > 100)
+                    {
+                printf("ERR: invalid value %d\n", val);
+                return EXIT_FAILURE;
+            }
+            return main_test_fan(val);
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+static void main_init_share(void) {
+    void* shm = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED,
+            -1, 0);
+    share_info = shm;
+    share_info->exit = 0;
+    share_info->cpu_temp = 0;
+    share_info->gpu_temp = 0;
+    share_info->fan_duty = 0;
+    share_info->fan_rpms = 0;
+    share_info->auto_duty = 1;
+    share_info->auto_duty_val = 0;
+    share_info->manual_next_fan_duty = 0;
+    share_info->manual_prev_fan_duty = 0;
+}
+
+static int main_ec_worker(void) {
+    setuid(0);
+    system("modprobe ec_sys");
+    while (share_info->exit == 0) {
+        // check parent
+        if (parent_pid != 0 && kill(parent_pid, 0) == -1) {
+            printf("worker on parent death\n");
+            break;
+        }
+        // write EC
+        int new_fan_duty = share_info->manual_next_fan_duty;
+        if (new_fan_duty != 0
+                && new_fan_duty != share_info->manual_prev_fan_duty) {
+            ec_write_fan_duty(new_fan_duty);
+            share_info->manual_prev_fan_duty = new_fan_duty;
+        }
+        // read EC
+        int io_fd = open("/sys/kernel/debug/ec/ec0/io", O_RDONLY, 0);
+        if (io_fd < 0) {
+            printf("unable to read EC from sysfs: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        unsigned char buf[EC_REG_SIZE];
+        ssize_t len = read(io_fd, buf, EC_REG_SIZE);
+        switch (len) {
+        case -1:
+            printf("unable to read EC from sysfs: %s\n", strerror(errno));
+            break;
+        case 0x100:
+            share_info->cpu_temp = buf[EC_REG_CPU_TEMP];
+            share_info->gpu_temp = buf[EC_REG_GPU_TEMP];
+            share_info->fan_duty = calculate_fan_duty(buf[EC_REG_FAN_DUTY]);
+            share_info->fan_rpms = calculate_fan_rpms(buf[EC_REG_FAN_RPMS_HI],
+                    buf[EC_REG_FAN_RPMS_LO]);
+            break;
+        default:
+            printf("wrong EC size from sysfs: %ld\n", len);
+        }
+        close(io_fd);
+        // auto EC
+        if (share_info->auto_duty == 1) {
+            int next_duty = ec_auto_duty_adjust();
+            if (next_duty != 0 && next_duty != share_info->auto_duty_val) {
+                char s_time[256];
+                get_time_string(s_time, 256, "%m/%d %H:%M:%S");
+                printf("%d\n", share_info->cpu_temp);
+                printf("%d\n", next_duty);
+                ec_write_fan_duty(next_duty);
+                share_info->auto_duty_val = next_duty;
+            }
+        }
+        //
+        usleep(200 * 1000);
+    }
+    //printf("worker quit\n");
+    return EXIT_SUCCESS;
+}
+
+static void main_ui_worker(int argc, char** argv) {
+    int desktop_uid = getuid();
+    setuid(desktop_uid);
+    //
+    gtk_init(&argc, &argv);
+    //
+    GtkWidget* indicator_menu = gtk_menu_new();
+
+    gtk_widget_show_all(indicator_menu);
+    // !! MAIN ON UI QUIT
+}
+
+static void main_on_sigchld(int signum) {
+    //printf("main on worker quit signal\n");
+    exit(EXIT_SUCCESS);
+}
+
+static void main_on_sigterm(int signum) {
+    printf("main on signal: %s\n", strsignal(signum));
+    if (share_info != NULL)
+        share_info->exit = 1;
+    exit(EXIT_SUCCESS);
+}
+
+static int show_temp(void) {
+   printf("%d°C\n", ec_query_cpu_temp());
+   return EXIT_SUCCESS;
+}
+
+static int fan_speed(void) {
+   printf("%d", ec_query_fan_rpms());
+   return EXIT_SUCCESS;
+}
+
+static int fan_duty(void) {
+   printf("%d", ec_query_fan_duty());
+   return EXIT_SUCCESS;
+}
+
+static int main_dump_fan(void) {
+    printf("Dump fan information\n");
+    printf("  FAN Duty: %d%%\n", ec_query_fan_duty());
+    printf("  FAN RPMs: %d RPM\n", ec_query_fan_rpms());
+    printf("  CPU Temp: %d°C\n", ec_query_cpu_temp());
+    printf("  GPU Temp: %d°C\n", ec_query_gpu_temp());
+    return EXIT_SUCCESS;
+}
+
+static int main_test_fan(int duty_percentage) {
+    printf("Change fan duty to %d%%\n", duty_percentage);
+    ec_write_fan_duty(duty_percentage);
+    printf("\n");
+    main_dump_fan();
+    return EXIT_SUCCESS;
+}
+
+static int ec_init(void) {
+    if (ioperm(EC_DATA, 1, 1) != 0)
+        return EXIT_FAILURE;
+    if (ioperm(EC_SC, 1, 1) != 0)
+        return EXIT_FAILURE;
+    return EXIT_SUCCESS;
+}
+
+static void ec_on_sigterm(int signum) {
+    printf("ec on signal: %s\n", strsignal(signum));
+    if (share_info != NULL)
+        share_info->exit = 1;
+}
+
+static int ec_auto_duty_adjust(void) {
+    int temp = MAX(share_info->cpu_temp, share_info->gpu_temp);
+    int duty = share_info->fan_duty;
+    //
+    if (temp >= 80 && duty < 100)
+        return 100;
+    if (temp >= 70 && duty < 90)
+        return 90;
+    if (temp >= 60 && duty < 80)
+        return 80;
+    if (temp >= 50 && duty < 70)
+        return 70;
+    if (temp >= 40 && duty < 60)
+        return 60;
+    if (temp >= 30 && duty < 50)
+        return 50;
+    if (temp >= 20 && duty < 40)
+        return 40;
+    if (temp >= 10 && duty < 30)
+        return 30;
+    //
+    if (temp <= 15 && duty > 30)
+        return 30;
+    if (temp <= 25 && duty > 40)
+        return 40;
+    if (temp <= 35 && duty > 50)
+        return 50;
+    if (temp <= 45 && duty > 60)
+        return 60;
+    if (temp <= 55 && duty > 70)
+        return 70;
+    if (temp <= 65 && duty > 80)
+        return 80;
+    if (temp <= 75 && duty > 90)
+        return 90;
+    //
+    return 0;
+}
+
+static int ec_query_cpu_temp(void) {
+    return ec_io_read(EC_REG_CPU_TEMP);
+}
+
+static int ec_query_gpu_temp(void) {
+    return ec_io_read(EC_REG_GPU_TEMP);
+}
+
+static int ec_query_fan_duty(void) {
+    int raw_duty = ec_io_read(EC_REG_FAN_DUTY);
+    return calculate_fan_duty(raw_duty);
+}
+
+static int ec_query_fan_rpms(void) {
+    int raw_rpm_hi = ec_io_read(EC_REG_FAN_RPMS_HI);
+    int raw_rpm_lo = ec_io_read(EC_REG_FAN_RPMS_LO);
+    return calculate_fan_rpms(raw_rpm_hi, raw_rpm_lo);
+}
+
+static int ec_write_fan_duty(int duty_percentage) {
+    if (duty_percentage < 10 || duty_percentage > 100) {
+        printf("Wrong fan duty to write: %d\n", duty_percentage);
+        return EXIT_FAILURE;
+    }
+    double v_d = ((double) duty_percentage) / 100.0 * 255.0;
+    int v_i = (int) v_d;
+    return ec_io_do(0x99, 0x01, v_i);
+}
+
+static int ec_io_wait(const uint32_t port, const uint32_t flag,
+        const char value) {
+    uint8_t data = inb(port);
+    int i = 0;
+    while ((((data >> flag) & 0x1) != value) && (i++ < 100)) {
+        usleep(1000);
+        data = inb(port);
+    }
+    if (i >= 100) {
+        printf("wait_ec error on port 0x%x, data=0x%x, flag=0x%x, value=0x%x\n",
+                port, data, flag, value);
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+static uint8_t ec_io_read(const uint32_t port) {
+    ec_io_wait(EC_SC, IBF, 0);
+    outb(EC_SC_READ_CMD, EC_SC);
+
+    ec_io_wait(EC_SC, IBF, 0);
+    outb(port, EC_DATA);
+
+    //wait_ec(EC_SC, EC_SC_IBF_FREE);
+    ec_io_wait(EC_SC, OBF, 1);
+    uint8_t value = inb(EC_DATA);
+
+    return value;
+}
+
+static int ec_io_do(const uint32_t cmd, const uint32_t port,
+        const uint8_t value) {
+    ec_io_wait(EC_SC, IBF, 0);
+    outb(cmd, EC_SC);
+
+    ec_io_wait(EC_SC, IBF, 0);
+    outb(port, EC_DATA);
+
+    ec_io_wait(EC_SC, IBF, 0);
+    outb(value, EC_DATA);
+
+    return ec_io_wait(EC_SC, IBF, 0);
+}
+
+static int calculate_fan_duty(int raw_duty) {
+    return (int) ((double) raw_duty / 255.0 * 100.0);
+}
+
+static int calculate_fan_rpms(int raw_rpm_high, int raw_rpm_low) {
+    int raw_rpm = (raw_rpm_high << 8) + raw_rpm_low;
+    return raw_rpm > 0 ? (2156220 / raw_rpm) : 0;
+}
+
+
+static void get_time_string(char* buffer, size_t max, const char* format) {
+    time_t timer;
+    struct tm tm_info;
+    time(&timer);
+    localtime_r(&timer, &tm_info);
+    strftime(buffer, max, format, &tm_info);
+}
+
+static void signal_term(__sighandler_t handler) {
+    signal(SIGHUP, handler);
+    signal(SIGINT, handler);
+    signal(SIGQUIT, handler);
+    signal(SIGPIPE, handler);
+    signal(SIGALRM, handler);
+    signal(SIGTERM, handler);
+    signal(SIGUSR1, handler);
+    signal(SIGUSR2, handler);
+}
+
